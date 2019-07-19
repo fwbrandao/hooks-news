263980364e93024ce024c94ede4b49cca39c9948
Fernando Brandao <fwbrandao@gmail.com>
2019-07-19 08:38:00 +0100

    getting data from news feed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function App() {
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks').then(response => {
+            console.log(response.data);
+            setResults(response.data.hits);
+        });
+    }, []);
+
   return (
-    <div>App</div>
+    <>
+    <ul>
+        {results.map(results => (
+            <li key={results.objectID}>
+                <a href={results.url}>{results.title}</a>
+            </li>
+        ))}
+    </ul>
+    </>
   );
 }
 
